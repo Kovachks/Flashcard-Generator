5539ec16dea34980fbfbbfa38ca93c6e8902e1e5
Keith Kovach <kovachks90@gmail.com>
2017-08-24 15:58:50 -0400

    finished

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,7 +1,9 @@
 var fs = require("fs")
 
 var ClozeCard = function(text, cloze) {
-	
+	this.cloze = cloze,
+	this.partial = text.replace(cloze, '...'),
+	this.fullText = text 
 }
 
 module.exports = ClozeCard
---
+++ frontend.js
@@ -0,0 +1,27 @@
+var BasicCard = require('./BasicCard')
+
+var ClozeCard = require('./ClozeCard')
+
+var firstPresident = new BasicCard(
+    "Who was the first president of the United States?", "George Washington");
+
+// "Who was the first president of the United States?"
+console.log("Basic Card Front: " + firstPresident.front); 
+
+// "George Washington"
+console.log("Basic Card Back: " + firstPresident.back); 
+
+var firstPresidentCloze = new ClozeCard(
+    "George Washington was the first president of the United States.", "George Washington");
+
+// "George Washington"
+console.log("Cloze Card Text: " + firstPresidentCloze.cloze); 
+
+// " ... was the first president of the United States.
+console.log("Cloze Card partial: " + firstPresidentCloze.partial);
+
+// "George Washington was the first president of the United States.
+console.log("Cloze Card Full Text: " + firstPresidentCloze.fullText);
+
+// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+var brokenCloze = new ClozeCard("This doesn't work", "oops");
